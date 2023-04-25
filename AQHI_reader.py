07176cb50d8c87b1b42f9f4e38e946c55da16bbb
wilsonleung767 <66161967+wilsonleung767@users.noreply.github.com>
2023-04-25 14:24:54 +0800

    2020-2022 AQHI monthly average

--- AQHI_reader.py
+++ AQHI_reader.py
@@ -7,11 +7,25 @@ def AQHI_reader(csv_file):
     csv_file_name = os.path.basename(csv_file)
     csv_file_date = csv_file_name[:6]
 
-    df = pd.read_csv(csv_file, header=None, usecols=[0,1] + list(range(3,19)), skiprows=8, na_values=['*'], parse_dates=[0])
+    df = pd.read_csv(csv_file, header=None, usecols=list(range(18)), skiprows=8, na_values=['*'], parse_dates=[0])
     df.columns = ['Date', 'Hour', 'Central/Western', 'Eastern', 'Kwun Tong', 'Sham Shui Po', 'Kwai Chung', 'Tsuen Wan', 'Tseung Kwan O', 'Yuen Long', 'Tuen Mun', 'Tung Chung', 'Tai Po', 'Sha Tin', 'Tap Mun', 'Causeway Bay', 'Central', 'Mong Kok']
     # Remove the time component from the Date column
     df['Date'] = df['Date'].dt.strftime('%Y-%m-%d')
-    df.to_excel(f'csv_to_excel/{csv_file_date}_AQHI.xlsx', index=False)
+    # Drop rows where 'Hour' column contains 'Daily Max'
+    df = df[df['Hour'] != 'Daily Max']
+    # Calculate the mean of each column and add to DataFrame
+    mean_row = df.mean(numeric_only=True).to_frame().T
+    mean_row['Date'] = f"{csv_file_date}"
+    mean_row['Hour'] = 'Monthly Average'
+    df = pd.concat([df, mean_row], ignore_index=True)
+
+    # Write DataFrame to Excel file
+    df.to_excel(f'csv_to_excel/AQHI_excel/{csv_file_date}_AQHI.xlsx', index=False)
     print('Excel file generated')
 
-AQHI_reader('data/202201_Eng.csv')
+csv_files = [f for f in os.listdir('data/AQHI_csv') if f.endswith('.csv')]
+
+# AQHI_reader('data\AQHI_csv/202001_Eng.csv')
+# loop through each CSV file and call the AQHI_reader function
+for csv_file in csv_files:
+    AQHI_reader(f"data/AQHI_csv/{csv_file}")
